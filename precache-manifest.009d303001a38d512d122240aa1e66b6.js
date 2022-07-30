6fd834efd3d3b4217330a6d7c05616f5307f0ea9
ErlJocson <jocsonerl@gmail.com>
2022-07-30 22:54:41 +0800

    Updates

---
+++ precache-manifest.009d303001a38d512d122240aa1e66b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "21f45288365ffca126a0",
+    "url": "/my-portfolio/static/css/main.13c16c25.chunk.css"
+  },
+  {
+    "revision": "21f45288365ffca126a0",
+    "url": "/my-portfolio/static/js/main.21f45288.chunk.js"
+  },
+  {
+    "revision": "80c59eb937da5769a111",
+    "url": "/my-portfolio/static/js/1.80c59eb9.chunk.js"
+  },
+  {
+    "revision": "f2bb3e8d5477afa4b7c6",
+    "url": "/my-portfolio/static/js/runtime~main.f2bb3e8d.js"
+  },
+  {
+    "revision": "bd3babb285862627b9094decddee8025",
+    "url": "/my-portfolio/index.html"
+  }
+];
