1415d0dea77f6a633eba4cef5c95ab1b78c8c45b
ErlJocson <jocsonerl@gmail.com>
2026-04-27 16:13:14 +0800

    added navigation bar functionality

--- src/components/section/NavigationBar.js
+++ src/components/section/NavigationBar.js
@@ -1,28 +1,34 @@
 import styled from "styled-components";
-import { FaHome } from "react-icons/fa";
 import { PiProjectorScreenChartFill } from "react-icons/pi";
 import { MdContactEmergency } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { FaHome, FaBars, FaTimes } from "react-icons/fa";
 
 function NavigationBar() {
+  const [showNav, setShowNav] = useState(true);
+
   return (
     <>
       <NavBar>
         <LeftItems>
           <img src={process.env.PUBLIC_URL + "/logo.png"} alt="logo" />
         </LeftItems>
-        <RightItems>
-          <MyLink to="/">
+
+        <BurgerButton onClick={() => setShowNav(!showNav)}>
+          {!showNav ? <FaTimes size={25} /> : <FaBars size={25} />}
+        </BurgerButton>
+
+        <RightItems showNav={showNav}>
+          <MyLink to="/" onClick={() => setShowNav(!showNav)}>
             <FaHome size={30} />
             ABOUT ME
           </MyLink>
-
-          <MyLink to="/projects">
+          <MyLink to="/projects" onClick={() => setShowNav(!showNav)}>
             <PiProjectorScreenChartFill size={30} />
             PROJECTS
           </MyLink>
-
-          <MyLink to="/contact-me">
+          <MyLink to="/contact-me" onClick={() => setShowNav(!showNav)}>
             <MdContactEmergency size={30} />
             CONTACT
           </MyLink>
@@ -56,6 +62,7 @@ const MyLink = styled(Link)`
 `;
 
 const NavBar = styled.div`
+  z-index: 100;
   overflow: hidden;
   height: 60px;
   background-color: rgb(2, 1, 16);
@@ -63,6 +70,10 @@ const NavBar = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  position: relative;
+  @media screen and (max-width: 1000px) {
+    overflow: visible;
+  }
 `;
 
 const NavigationItems = styled.div`
@@ -70,10 +81,10 @@ const NavigationItems = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin: 0px 30px;
 `;
 
 const LeftItems = styled(NavigationItems)`
+  margin: 0 30px;
   img {
     width: 30px;
     margin: 0 10px;
@@ -82,4 +93,32 @@ const LeftItems = styled(NavigationItems)`
 
 const RightItems = styled(NavigationItems)`
   display: flex;
+  margin: 0 30px;
+
+  @media screen and (max-width: 1000px) {
+    background-color: rgb(2, 1, 16);
+    margin: 0;
+    z-index: 100;
+    height: 100vh;
+    display: ${(props) => (props.showNav ? "none" : "flex")};
+    flex-direction: column;
+    position: fixed;
+    bottom: 0;
+    top: 0;
+    left: 0;
+    right: 0;
+    align-items: flex-start;
+    justify-content: flex-start;
+  }
+`;
+
+const BurgerButton = styled.div`
+  z-index: 101;
+  display: none;
+  margin-right: 30px;
+  color: white;
+  cursor: pointer;
+  @media screen and (max-width: 1000px) {
+    display: block;
+  }
 `;
